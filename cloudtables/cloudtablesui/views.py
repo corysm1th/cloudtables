0e895e145a36cfed2bf152158cdf0bf84081337f
Cory Smith <smith.cory@gmail.com>
2018-03-25 01:46:22 -0700

    List out regions

--- cloudtables/cloudtablesui/views.py
+++ cloudtables/cloudtablesui/views.py
@@ -12,8 +12,7 @@ def index(request):
 
 def sync(request):
 	awscreds = ConfigParser.ConfigParser()
-	#awsregions = ['us-west-1', 'us-west-2', 'us-east-1']
-	awsregions = ec2.regions()
+	awsregions = ['us-west-1', 'us-west-2', 'us-east-1', 'ap-northeast-1', 'ap-southeast-2', 'sa-east-1', 'ap-northeast-2', 'us-east-2', 'ap-southeast-1', 'ca-central-1', 'ap-south-1', 'eu-central-1', 'eu-west-1', 'eu-west-2']
 	awscreds.read(['credentials', os.path.expanduser('~/.aws/credentials')])
 
 	for section in awscreds.sections():

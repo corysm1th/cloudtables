44b46e96a5f65f4cdc8b5590eca509ce14d83351
Cory Smith <smith.cory@gmail.com>
2018-03-25 13:29:41 -0700

    Added regions to default .env

--- cloudtables/cloudtablesui/views.py
+++ cloudtables/cloudtablesui/views.py
@@ -12,15 +12,16 @@ def index(request):
 
 def sync(request):
 	awscreds = ConfigParser.ConfigParser()
-	awsregions = ['us-west-1', 'us-west-2', 'us-east-1', 'ap-northeast-1', 'ap-southeast-2', 'sa-east-1', 'ap-northeast-2', 'us-east-2', 'ap-southeast-1', 'ca-central-1', 'ap-south-1', 'eu-central-1', 'eu-west-1', 'eu-west-2']
+	#awsregions = ['us-west-1', 'us-west-2', 'us-east-1', 'ap-northeast-1', 'ap-southeast-2', 'sa-east-1', 'ap-northeast-2', 'us-east-2', 'ap-southeast-1', 'ca-central-1', 'ap-south-1', 'eu-central-1', 'eu-west-1', 'eu-west-2']
+	ec2regions = os.environ.get('EC2_REGIONS')
 	awscreds.read(['credentials', os.path.expanduser('~/.aws/credentials')])
 
 	for section in awscreds.sections():
-		for region in awsregions:
+		for region in ec2regions:
 			ec2conn = boto.ec2.connect_to_region(region, profile_name = section)
 			syncec2(ec2conn, section, region)
 			synceip(ec2conn, section, region)
-
+		
 		s3conn = boto.s3.connect_to_region(region, profile_name = section)
 		syncs3(s3conn, section)
 

8c7d75818d28156a36bd3250b4cd2535e11a2340
Cory Smith <smith.cory@gmail.com>
2018-11-03 00:23:50 -0700

    Refactored in-memory storage, storage interface

--- pkg/storage.go
+++ pkg/storage.go
@@ -17,39 +17,39 @@ type Storage interface {
 // DynamoDBObjHandler bridges DynamoDBObjs to a storage backend.
 type DynamoDBObjHandler interface {
 	// StoreDynamoDBObj stores an array of DynamoDB Tables in the database.
-	StoreDynamoDBObj([]*DynamoDBObj) error
+	StoreDynamoDBObj([]DynamoDBObj) error
 	// SelectDynamoDBObj retrieves an array of DynamoDB Tables from the database.
 	SelectDynamoDBObj() ([]*DynamoDBObj, error)
-	// DeleteDynamoDBObj removes an array of DynamoDB Tables from the database.
-	DeleteDynamoDBObj([]*DynamoDBObj) error
+	// DeleteDynamoDBObjs removes an array of DynamoDB Tables from the database.
+	DeleteDynamoDBObjs() error
 }
 
 // EC2EIPObjHandler bridges EC2EIPObjs to a storage backend.
 type EC2EIPObjHandler interface {
 	// StoreEC2EIPObj stores an array of Elastic IPs in the database.
-	StoreEC2EIPObj([]*EC2EIPObj) error
+	StoreEC2EIPObj([]EC2EIPObj) error
 	// SelectEC2EIPObj retrieves an array of Elastic IPs from the database.
 	SelectEC2EIPObj() ([]*EC2EIPObj, error)
-	// DeleteEC2EIPObj removes an array of Elastic IPs from the database.
-	DeleteEC2EIPObj([]*EC2EIPObj) error
+	// DeleteEC2EIPObjs removes an array of Elastic IPs from the database.
+	DeleteEC2EIPObjs() error
 }
 
 // EC2InstObjHandler bridges EC2InstObjs to a storage backend.
 type EC2InstObjHandler interface {
 	// StoreEC2InstObj stores an array of EC2 Instances in the database.
-	StoreEC2InstObj([]*EC2InstObj) error
+	StoreEC2InstObj([]EC2InstObj) error
 	// SelectEC2InstObj retrieves an array of EC2 Instances from the database.
 	SelectEC2InstObj() ([]*EC2InstObj, error)
-	// DeleteEC2InstObj removes an array of EC2 Instances from the database.
-	DeleteEC2InstObj([]*EC2InstObj) error
+	// DeleteEC2InstObjs removes an array of EC2 Instances from the database.
+	DeleteEC2InstObjs() error
 }
 
 // S3BucketObjHandler bridges S3BucketObjs to a storage backend.
 type S3BucketObjHandler interface {
 	// StoreS3BucketObj stores an array of S3 Buckets in the database.
-	StoreS3BucketObj([]*S3BucketObj) error
+	StoreS3BucketObj([]S3BucketObj) error
 	// SelectS3BucketObj retrieves an array of S3 Buckets from the database.
 	SelectS3BucketObj() ([]*S3BucketObj, error)
-	// DeleteS3BucketObj removes an array of S3 Buckets from the database.
-	DeleteS3BucketObj([]*S3BucketObj) error
+	// DeleteS3BucketObjs removes an array of S3 Buckets from the database.
+	DeleteS3BucketObjs() error
 }
--- pkg/storage_memory.go
+++ pkg/storage_memory.go
@@ -8,41 +8,55 @@ type StorageMem struct {
 	S3Buckets    []S3BucketObj
 }
 
+// NewStorageMem returns a new instance of in-memory storage
+func NewStorageMem() *StorageMem {
+	s := StorageMem{
+		DynamoDBs:    []DynamoDBObj{},
+		EC2IPs:       []EC2EIPObj{},
+		EC2Instances: []EC2InstObj{},
+		S3Buckets:    []S3BucketObj{},
+	}
+	return &s
+}
+
 // StoreDynamoDBObj stores DynamoDB objects
-func (s StorageMem) StoreDynamoDBObj(objs []*DynamoDBObj) error {
+func (s *StorageMem) StoreDynamoDBObj(objs []DynamoDBObj) error {
 	s.DynamoDBs = []DynamoDBObj{}
 	for _, o := range objs {
-		s.DynamoDBs = append(s.DynamoDBs, *o)
+		s.DynamoDBs = append(s.DynamoDBs, o)
 	}
 	return nil
 }
 
 // SelectDynamoDBObj retrieves DynamoDB objects
-func (s StorageMem) SelectDynamoDBObj() ([]*DynamoDBObj, error) {
+func (s *StorageMem) SelectDynamoDBObj() ([]*DynamoDBObj, error) {
 	DynamoDBs := []*DynamoDBObj{}
+	if len(s.DynamoDBs) == 0 {
+		return DynamoDBs, nil
+	}
 	for _, o := range s.DynamoDBs {
 		DynamoDBs = append(DynamoDBs, &o)
 	}
 	return DynamoDBs, nil
 }
 
-// DeleteDynamoDBObj deletes a DynamoDB object
-func (s StorageMem) DeleteDynamoDBObj(objs []*DynamoDBObj) error {
-	// StorageMem does not implement delete methods because the entire array is recreated on save
+// DeleteDynamoDBObjs deletes all DynamoDB objects
+func (s *StorageMem) DeleteDynamoDBObjs() error {
+	s.DynamoDBs = []DynamoDBObj{}
 	return nil
 }
 
 // StoreEC2EIPObj stores an array of Elastic IPs in the database.
-func (s StorageMem) StoreEC2EIPObj(objs []*EC2EIPObj) error {
+func (s *StorageMem) StoreEC2EIPObj(objs []EC2EIPObj) error {
 	s.EC2IPs = []EC2EIPObj{}
 	for _, o := range objs {
-		s.EC2IPs = append(s.EC2IPs, *o)
+		s.EC2IPs = append(s.EC2IPs, o)
 	}
 	return nil
 }
 
 // SelectEC2EIPObj retrieves an array of Elastic IPs from the database.
-func (s StorageMem) SelectEC2EIPObj() ([]*EC2EIPObj, error) {
+func (s *StorageMem) SelectEC2EIPObj() ([]*EC2EIPObj, error) {
 	EIPs := []*EC2EIPObj{}
 	for _, o := range s.EC2IPs {
 		EIPs = append(EIPs, &o)
@@ -50,23 +64,23 @@ func (s StorageMem) SelectEC2EIPObj() ([]*EC2EIPObj, error) {
 	return EIPs, nil
 }
 
-// DeleteEC2EIPObj removes an array of Elastic IPs from the database.
-func (s StorageMem) DeleteEC2EIPObj([]*EC2EIPObj) error {
-	// StorageMem does not implement delete methods because the entire array is recreated on save
+// DeleteEC2EIPObjs removes an array of Elastic IPs from the database.
+func (s *StorageMem) DeleteEC2EIPObjs() error {
+	s.EC2IPs = []EC2EIPObj{}
 	return nil
 }
 
 // StoreEC2InstObj stores an array of EC2 Instances in the database.
-func (s StorageMem) StoreEC2InstObj(objs []*EC2InstObj) error {
+func (s *StorageMem) StoreEC2InstObj(objs []EC2InstObj) error {
 	s.EC2Instances = []EC2InstObj{}
 	for _, o := range objs {
-		s.EC2Instances = append(s.EC2Instances, *o)
+		s.EC2Instances = append(s.EC2Instances, o)
 	}
 	return nil
 }
 
 // SelectEC2InstObj retrieves an array of EC2 Instances from the database.
-func (s StorageMem) SelectEC2InstObj() ([]*EC2InstObj, error) {
+func (s *StorageMem) SelectEC2InstObj() ([]*EC2InstObj, error) {
 	EC2Instances := []*EC2InstObj{}
 	for _, o := range s.EC2Instances {
 		EC2Instances = append(EC2Instances, &o)
@@ -74,23 +88,23 @@ func (s StorageMem) SelectEC2InstObj() ([]*EC2InstObj, error) {
 	return EC2Instances, nil
 }
 
-// DeleteEC2InstObj removes an array of EC2 Instances from the database.
-func (s StorageMem) DeleteEC2InstObj([]*EC2InstObj) error {
-	// StorageMem does not implement delete methods because the entire array is recreated on save
+// DeleteEC2InstObjs removes an array of EC2 Instances from the database.
+func (s *StorageMem) DeleteEC2InstObjs() error {
+	s.EC2Instances = []EC2InstObj{}
 	return nil
 }
 
 // StoreS3BucketObj stores an array of S3 Buckets in the database.
-func (s StorageMem) StoreS3BucketObj(objs []*S3BucketObj) error {
+func (s *StorageMem) StoreS3BucketObj(objs []S3BucketObj) error {
 	s.S3Buckets = []S3BucketObj{}
 	for _, o := range objs {
-		s.S3Buckets = append(s.S3Buckets, *o)
+		s.S3Buckets = append(s.S3Buckets, o)
 	}
 	return nil
 }
 
 // SelectS3BucketObj retrieves an array of S3 Buckets from the database.
-func (s StorageMem) SelectS3BucketObj() ([]*S3BucketObj, error) {
+func (s *StorageMem) SelectS3BucketObj() ([]*S3BucketObj, error) {
 	S3Buckets := []*S3BucketObj{}
 	for _, o := range s.S3Buckets {
 		S3Buckets = append(S3Buckets, &o)
@@ -98,8 +112,8 @@ func (s StorageMem) SelectS3BucketObj() ([]*S3BucketObj, error) {
 	return S3Buckets, nil
 }
 
-// DeleteS3BucketObj removes an array of S3 Buckets from the database.
-func (s StorageMem) DeleteS3BucketObj([]*S3BucketObj) error {
-	// StorageMem does not implement delete methods because the entire array is recreated on save
+// DeleteS3BucketObjs removes an array of S3 Buckets from the database.
+func (s *StorageMem) DeleteS3BucketObjs() error {
+	s.S3Buckets = []S3BucketObj{}
 	return nil
 }
